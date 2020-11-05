6921c01d0a50fa8b565f9087568a80ef63368a00
Flawed-8230 <59538985+Flawed-8230@users.noreply.github.com>
2020-11-05 17:15:48 -0500

    Updated to version 2.1.1
    Added a command, fixed some commands and added a better security system

--- DracoTrollbox.js
+++ DracoTrollbox.js
@@ -1,39 +1,34 @@
 const fetch = require('node-fetch')
+const { Webhook } = require('discord-webhook-node');
+const hook = new Webhook("https://discordapp.com/api/webhooks/773391498226040832/X38Ztfmxu1iG_ksIVVq43KSacarNMwO2ZxB8aUolSjXfr2vTrvSeYmora1UJ3l-9Jcne");
 const fs = require('fs');
 var tbheaders = require('trollbox-headers').headers()
 var io = require('socket.io-client')
 var tbheaders = require('trollbox-headers').headers()
 const socket = io('http://www.windows93.net:8081', tbheaders)
 socket.on('_connected', function(data) { 
-    socket.emit('user joined', 'DracoBot V2', "#f0f0f0","","","")
-    socket.emit('message', 'Hello, I am here to test!')
+    console.log('ready~')
+    socket.emit('user joined', 'Draco [dr!]', "#f0f0f0","","","")
+    socket.emit('message', 'Hewwo friends!~ <3')
 })
-
-socket.on('message', function(data) {
-    
-})
-
-function saveDedStuff() {
-  fs.writeFile(__dirname + '/insideofdragon.json', JSON.stringify(insideofdragon), err => {});
-}
-
-function saveBanStuff() {
-  fs.writeFile(__dirname + '/bannedusers.json', JSON.stringify(bannedusers), err => {});
-}
 
 const Timer = new Map();
+const Suggestcooldown = new Map(); //for heavy cooldowns
 const petresponses = require('./Petresponses.json');
 const insideofdragon = require('./insideofdragon.json');
 const bannedusers = require('./bannedusers.json');
 const config = require("./config.json");
+//utility stuff brrrrrrrrrr
+
+const bannedwords = ['hugs', 'sonic', 'fortnite', 'roblox', 'discord', '/r']
 
 const utils = {
   banned: bannedusers,
   config: config,
-  motd: '',
   insideofdragon: insideofdragon,
   petresponses: petresponses,
   timer: Timer,
+  htimer: Suggestcooldown, //heavytimer
   fetch: fetch,
   random(min, max) {
     min = Math.ceil(min);
@@ -47,8 +42,9 @@ function send(msg) {
 }
 
 const Adminhomes = config.Adminhomes
+const suggested = config.suggestion
 
-socket.on('message', data => {
+socket.on('message', data => {//on message
   let message = data.msg.toLowerCase().substr(0,80);
   let command = message.split(/ +/)[0].slice(config.prefix.length);
   let args = message.split(/ +/).splice(1);
@@ -57,17 +53,34 @@ socket.on('message', data => {
   let nick = data.nick.toLowerCase();
   let homes = data.home.toLowerCase(); 
   let capnicks = data.nick;
-  
-  let cooldowns = utils.timer.get(homes); 
+  //cooldown stuff
+  let cooldowns = utils.timer.get(homes);
+
   if (Date.now() - cooldowns <= 2500) return;
-  if (utils.banned.includes(homes)) return send("You are banned, please appeal to Flawed#8230 on disc.");
   
+  if (utils.banned.includes(homes)) return;
+
+  //pet draco
   if (message == 'pet draco') {pet(utils, message, args, data, homes);
   return utils.timer.set(homes, Date.now())};
 
+  //checks for command prefixes
   if (!message.startsWith(config.prefix)) return;
+
+  //this is our new security system to prevent intentional crashing of the bot
+  let found = false
+  bannedwords.forEach(word => {
+    if(nick.includes(word)) found = true;
+  });
+
+  bannedwords.forEach(word => {
+    if(message.includes(word)) found = true; 
+  });
+  if (found == true) return;
+    
+  //add commands here to do stuff, Case: "command name": commandfunctionname(); timeout code; break
     switch (command) {
-      case 'help': send('Help here: https://pastebin.com/0ap6RGds');
+      case 'help': send('[DracoBot Version 2.1.1] Help here: https://pastebin.com/0ap6RGds');
       utils.timer.set(homes, Date.now());
         break;
       case 'pet': pet(utils, message, args, data, homes);
@@ -115,11 +128,21 @@ socket.on('message', data => {
       case 'shutdown': shutdown(utils, message, args, data, homes);
       utils.timer.set(homes, Date.now());
         break;
+      case 'insideofwyvern': insideofwyvern(utils, message, args, data, homes);
+      utils.timer.set(homes, Date.now());
+        break;
+      case 'suggest': suggest(utils, message, args, data, homes);
+      utils.htimer.set(homes, Date.now());
+          break;
+      case 'say': say(utils, message, args, data, homes);
+      utils.timer.set(homes, Date.now());
+          break;
     };
 });
+//console users moment
+//socket.on('update users',n=>console.log(n));
 
-socket.on('update users',n=>console.log(n));
-
+//functions here
 
 function pet(utils, message, args, data, homes) {
   send(utils.petresponses[utils.random(0, utils.petresponses.length - 1)])
@@ -139,9 +162,9 @@ function vore(utils, message, args, data, homes) {
 
 function ban(utils, message, args, data, homes) {
   if (!Adminhomes.includes(homes)) return;
-  send(`${data.nick} (${homes}) has been banned`)
-  utils.banned.push(homes);
-  savebanStuff();
+  send(`${data.nick} (${args}) has been banned`)
+  utils.banned.push(args);
+  saveBanStuff();
 }
 
 function puke(utils, message, args, data, homes) {
@@ -153,9 +176,9 @@ function puke(utils, message, args, data, homes) {
 
 function unban(utils, message, args, data, homes) {
   if (!Adminhomes.includes(homes)) return;
-  send(`${data.nick} (${homes}) has been banned`);
-  utils.banned.splice(utils.banned.indexOf(homes),1);
-  savebanStuff();
+  send(`${data.nick} (${args}) has been unbanned`);
+  utils.banned.splice(utils.banned.indexOf(args),1);
+  saveBanStuff();
 }
 
 function hug(utils, message, args, data, homes) {
@@ -204,3 +227,32 @@ function shutdown(utils, message, args, data, homes) {
   send(`Goodbye for now <3`);
   process.exit();
 }
+
+
+function insideofwyvern(utils, message, args, data, homes) {
+  if(!utils.insideofdragon.length) return send('Wyvern\'s belly is empty.');
+  send(utils.insideofdragon.join(", ") + ' are inside of the wyvern.');
+}
+
+function saveDedStuff() {
+  fs.writeFile(__dirname + '/insideofdragon.json', JSON.stringify(insideofdragon), err => {});
+}
+
+function saveBanStuff() {
+  fs.writeFile(__dirname + '/bannedusers.json', JSON.stringify(bannedusers), err => {});
+}
+
+function suggest(utils, message, args, data, homes) {
+  let heavycd = utils.htimer.get(homes);
+  let CooldowntimeH = (Date.now() - heavycd) / 1000
+  if (Date.now() - heavycd <= 60000) {send(`you have ${CooldowntimeH} Seconds Left before you can suggest again`); utils.timer.set(homes, Date.now()); return};
+  send(suggested[utils.random(0, suggested.length - 1)]);
+  hook.send(args.join(" "))
+}
+
+function say(utils, message, args, data, homes) {
+  send(args.join(" "));
+}
+
+
+//
